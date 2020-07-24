2bea7d79ca183e8f84c9aea2784ab70c816c02aa
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-07-24 18:38:02 +0700

    first commit

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-/**
- * @author Pamontep Panya
- * @email pamontep.p@gmail.com
- * @create date 2018-06-03 03:45:02
- * @modify date 2018-06-03 03:45:02
- * @desc A sample project of Node.js and Line API
- */
-const server = require("express");
-const PORT = process.env.PORT || 9999;
-const request = require("request");
+const express = require("express");
 const bodyParser = require("body-parser");
-
-server()
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: false }))
-  .get("/", (req, res) =>
-    res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
-  )
-  .post("/webhook", function (req, res) {
-    let replyToken = req.body.events[0].replyToken;
-    let msg = req.body.events[0].message.text;
-
-    console.log(`Message token : ${replyToken}`);
-    console.log(`Message from chat : ${msg}`);
-
-    res.json({
-      status: 200,
-      message: `Webhook is working!`,
-    });
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const request = require("request");
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post("/webhook", (req, res) => {
+  let reply_token = req.body.events[0].replyToken;
+  let msg = req.body.events[0].message.text;
+  reply(reply_token, msg);
+  res.sendStatus(200);
+});
+app.listen(port);
+function reply(reply_token, msg) {
+  let headers = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer {KgShB29fTkZDN1AbVTfZok/t+EXmcHh8eZvF306SbZCKwiGLg68l/jCfkf3FihSo7oMU4FjplLFCnb9OlOZh61i7TuUu9FyYdogAPuxmqnBgjdFpv8bQ874xMbcNdyCaQrQALkiFRGt960WrO0L1egdB04t89/1O/w1cDnyilFU=}",
+  };
+  let body = JSON.stringify({
+    replyToken: reply_token,
+    messages: [
+      {
+        type: "text",
+        text: msg,
+      },
+    ],
+  });
+  request.post(
+    {
+      url: "https://api.line.me/v2/bot/message/reply",
+      headers: headers,
+      body: body,
+    },
+    (err, res, body) => {
+      console.log("status = " + res.statusCode);
+    }
+  );
+}
