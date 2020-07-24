fdd1a8be253909d65b09e6056f44f16bf570ea7c
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-07-24 19:07:44 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
 const port = process.env.PORT || 4000;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.post("/webhook", (req, res) => {
