6a3ec965dc1988839db8c1795f56e73189e3533c
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-07-24 17:48:44 +0700

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const server = require("express");
+const PORT = process.env.PORT || 9999;
 const request = require("request");
 const bodyParser = require("body-parser");
 
@@ -8,16 +9,4 @@ server()
   .get("/", (req, res) =>
     res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
   )
-  .post("/webhook", function (req, res) {
-    let replyToken = req.body.events[0].replyToken;
-    let msg = req.body.events[0].message.text;
-
-    console.log(`Message token : ${replyToken}`);
-    console.log(`Message from chat : ${msg}`);
-
-    res.json({
-      status: 200,
-      message: `Webhook is working!`,
-    });
-  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
