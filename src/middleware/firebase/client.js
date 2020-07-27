bddf231c65a7082cde0e251fe8c58826030bd796
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-07-28 02:16:47 +0700

    Add env

--- src/middleware/firebase/client.js
+++ src/middleware/firebase/client.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+// const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
   databaseURL: process.env.DATABASE_URL,
 });
 
